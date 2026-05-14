6b2253191ef6bcdb1c3e25ffb0cbb87652504fc8
ShiriSalman <165762263+ShiriSalman@users.noreply.github.com>
2026-05-14 23:53:35 +0200

    Add Company and Analyzer classes

---
+++ analyzer.py
@@ -0,0 +1,20 @@
+class Company:
+
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+        
+
+class Analyzer:
+
+    def __init__(self, company_list):
+        self.company_list = company_list
+
+def analyze_company(self):
+    pass
+
+def calculate_esg_score(self):
+    pass
+
+def ranking(self):
+    ...
